ff8d46550a2c50e47afdac6b8d81dfed16d1b169
studio3104 <studio3104.com@gmail.com>
2016-04-11 23:16:54 +0900

    follow the changing of settings

--- tests/settings_test.py
+++ tests/settings_test.py
@@ -4,6 +4,6 @@ from linebot.settings import API_URL_EVENTS, API_URL_PROFILES, API_URL_BOT
 
 
 def test_default_urls():
-    assert API_URL_EVENTS == 'https://api.line.me/v1/events'
-    assert API_URL_PROFILES == 'https://api.line.me/v1/profiles'
-    assert API_URL_BOT == 'https://api.line.me/v1/bot'
+    assert API_URL_EVENTS == 'https://trialbot-api.line.me/v1/events'
+    assert API_URL_PROFILES == 'https://trialbot-api.line.me/v1/profiles'
+    assert API_URL_BOT == 'https://trialbot-api.line.me/v1/bot'
